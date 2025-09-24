48719bc1cf36eaf0e744752e92634dcf946d33a1
BahaaShk <bahaa.shk@gmail.com>
2025-09-24 16:50:03 +0300

    first commit with part 1 course info done

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+const handleGood = () => {
+  setGood(prev => prev + 1)
+}
+const handleNeutral = () => {
+  setNeutral(prev => prev + 1)
+}
+const handleBad = () => {
+  setBad(prev => prev + 1)
+}
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h2>statistics</h2>
+      <p>good : {good}</p>
+      <p>neutral :{neutral}</p>
+      <p>bad : {bad}</p>
+    </div>
+  );
+};
+
+export default App;
