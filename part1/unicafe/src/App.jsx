95a0484f7ee981d654f03d802988dec3275728ba
BahaaShk <bahaa.shk@gmail.com>
2025-09-24 22:24:30 +0300

    unicafe step 3 and 4 are done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,39 @@
 import { useState } from "react";
 
+const Statistics = ({handleBad,handleGood,handleNeutral, good, bad, neutral,total, average}) => {
+  if (total === 0) {
+    return(
+      <>
+      <h2>give feedback</h2>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h2>statistics</h2>
+<p>no feedback given</p> 
+      </>
+    )
+  } else
+  return (
+
+    
+    <>
+
+      <h2>give feedback</h2>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average { total === 0 ? 0 :  average / total}</p>
+      <p>positive { total === 0 ?  0 : (good / total)*100 } %</p>
+      </>
+    
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -17,20 +51,18 @@ const App = () => {
   };
 
 const total = good + bad + neutral;
+const average = (good*1 + neutral*0 + bad*(-1))
 
   return (
     <div>
-      <h2>give feedback</h2>
-      <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {(good*1 + neutral*0 + bad*(-1)) / total} </p>
-      <p>positive {(good / total)*100} %</p>
+<Statistics good={good}
+  neutral={neutral}
+  bad={bad}
+  total={total}
+  average={average}
+  handleGood={handleGood}
+  handleNeutral={handleNeutral}
+  handleBad={handleBad} />
     </div>
   );
 };
