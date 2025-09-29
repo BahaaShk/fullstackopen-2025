a732fbfc2b06195f8ec28362a123c7924885b83e
BahaaShk <bahaa.shk@gmail.com>
2025-09-29 13:53:35 +0300

    finished exercise 2.1

---
+++ part2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = (props) => {
+  return (
+        <>
+        {
+          props.parts.map((part,i) => {
+            return (
+              <div key={i}>
+                <Part part={part.name} exercises={part.exercises} />
+              </div>
+            )
+          })
+        }
+      {/* <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} /> */}
+    </>
+  )
+}
+
+export default Content
