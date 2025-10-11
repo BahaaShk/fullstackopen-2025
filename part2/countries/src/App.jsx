d53e831d6c177ae0f61e7cf948a21253af9c39ce
BahaaShk <bahaa.shk@gmail.com>
2025-10-11 22:28:33 +0300

    exercise 2.19 is finished

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -5,13 +5,14 @@ import { useState } from "react";
 function App() {
   const [filter, setFilter] = useState("");
   const [countries, setCountries] = useState([]);
+  const [showCountry, setShowCountry] = useState(null);
 
   const BaseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all";
 
   useEffect(() => {
     axios
       .get(BaseUrl)
-      .then((res) => {
+      .then((res) => {   
         setCountries(res.data);
       })
       .catch((error) => {
@@ -30,38 +31,62 @@ function App() {
         <input
           type="text"
           value={filter}
-          onChange={(e) => setFilter(e.target.value)}
+          onChange={(e) =>
+            {
+              setFilter(e.target.value)
+setShowCountry(null)
+            } 
+          }
         />
       </div>
       <div>
         {filteredCountries.length > 10 ? (
           <p>Too many matches. specify another filter</p>
         ) : filteredCountries.length > 1 ? (
-          
-            filteredCountries.map((country,index) => (
-              
-              <p key={index}>{country.name.common}</p>
-              
-            ))
-          
-        ) :  filteredCountries.length === 1 ? (
+          <>
+            {filteredCountries.map((country, index) => (
+              <p key={index}>
+                {country.name.common}
+                <button onClick={() =>{
+                  setShowCountry(country)
+                  console.log(country.name.common);
+                  
+                } 
+                }>Show</button>
+                  
+              </p>
+            ))}
+            {showCountry && (
+              <div>
+                <h3>{showCountry.name.common}</h3>
+                <p>Capital {showCountry.capital}</p>
+                <p>Area {showCountry.area}</p>
+                <h4>Languages</h4>
+                <ul>
+                  {Object.values(showCountry.languages).map((lang) => (
+                    <li key={lang}>{lang}</li>
+                  ))}
+                </ul>
+                <img src={showCountry.flags.png} alt="flag" width="180" />
+              </div>
+            )}
+          </>
+        ) : filteredCountries.length === 1 ? (
           <div>
- <h3>{filteredCountries[0].name.common}</h3>
-          <p>Capital {filteredCountries[0].capital}</p>
-          <p>Area {filteredCountries[0].area}</p>
-          <h4>Languages</h4>
-       <ul>
-  {Object.values(filteredCountries[0].languages).map((lang) => (
-    <li key={lang}>{lang}</li>
-  ))}
-</ul>
-          <img
-            src={filteredCountries[0].flags.png}
-            alt="flag"
-            width="180"
-          />
+            <h3>{filteredCountries[0].name.common}</h3>
+            <p>Capital {filteredCountries[0].capital}</p>
+            <p>Area {filteredCountries[0].area}</p>
+            <h4>Languages</h4>
+            <ul>
+              {Object.values(filteredCountries[0].languages).map((lang) => (
+                <li key={lang}>{lang}</li>
+              ))}
+            </ul>
+            <img src={filteredCountries[0].flags.png} alt="flag" width="180" />
           </div>
-        ) : (<p>no matches</p>)}
+        ) : (
+          <p>no matches</p>
+        )}
       </div>
     </>
   );
