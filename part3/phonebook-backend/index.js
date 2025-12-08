7b0023142192fd183f1f9e3a5103b857d07fc743
BahaaShk <bahaa.shk@gmail.com>
2025-12-08 23:53:12 +0200

    exercise 3.4 is done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -25,20 +25,31 @@ let persons = [
 ];
 
 app.get("/info", (request, response) => {
-  const time = new Date()
+  const time = new Date();
   response.send(`<p>Phonebook has info for ${persons.length} people</p>
      <p>${time}</p>`);
 });
 
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id)
+  const person = persons.find((person) => person.id === id);
 
-  if(person){
+  if (person) {
     response.json(person);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
