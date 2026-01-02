42a7f9079521db7dab121e39f4a80b9cbb7fa53c
BahaaShk <bahaa.shk@gmail.com>
2026-01-02 15:11:34 +0200

    finished exercise 4.4

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,9 +1,70 @@
-const { test, describe } = require('node:test');
-const assert = require('node:assert');
-const listHelper = require('../utils/list_helper');
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const listHelper = require("../utils/list_helper");
 
-test('dummy returns one', () => {
+test("dummy returns one", () => {
   const blogs = [];
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
+});
+
+describe("total likes", () => {
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when list has only one blog equals the likes of that", () => {
+    const listWithOneBlog = [
+      {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        likes: 5,
+      },
+    ];
+
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const blogs = [
+      {
+        title: "React patterns",
+        author: "Michael Chan",
+
+        likes: 7,
+      },
+      {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        likes: 5,
+      },
+      {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+
+        likes: 12,
+      },
+      {
+        title: "First class tests",
+        author: "Robert C. Martin",
+
+        likes: 10,
+      },
+      {
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+
+        likes: 0,
+      },
+      {
+        title: "Type wars",
+        author: "Robert C. Martin",
+        likes: 2,
+      },
+    ];
+    const result = listHelper.totalLikes(blogs);
+    assert.strictEqual(result, 36);
+  });
 });
